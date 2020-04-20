064b6d74c0f59011a1dc1651e0304bbc16646bd8
duyue6002 <duyue6002@gmail.com>
2020-04-20 22:08:43 +0800

    finish!

--- src/containers/AddTodo.js
+++ src/containers/AddTodo.js
@@ -1,27 +1,29 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { addTodo } from '../actions'
+import React from "react";
+// import { connect } from "react-redux";
+import { connect } from "../lib/react-redux";
+import { addTodo } from "../actions";
 
 const AddTodo = ({ dispatch }) => {
-  let input
+  let input;
 
   return (
     <div>
-      <form onSubmit={e => {
-        e.preventDefault()
-        if (!input.value.trim()) {
-          return
-        }
-        dispatch(addTodo(input.value))
-        input.value = ''
-      }}>
-        <input ref={node => input = node} />
-        <button type="submit">
-          Add Todo
-        </button>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (!input.value.trim()) {
+            return;
+          }
+          dispatch(addTodo(input.value));
+          input.value = "";
+        }}>
+        <input ref={(node) => (input = node)} />
+        <button type="submit">Add Todo</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default connect()(AddTodo)
+const mapDispatchToProps = (dispatch) => ({ dispatch });
+
+export default connect(null, mapDispatchToProps)(AddTodo);
---
+++ src/lib/react-redux.js
@@ -0,0 +1,69 @@
+import React, { useMemo, useState, useEffect } from "react";
+import { ReactReduxContext } from "react-redux";
+
+// const ReactReduxContext = React.createContext(null);
+
+/**
+ * Provider 函数组件
+ * 接受参数： props - {store，context，children }
+ * 返回：ContextProvider组件，value是store和subscription
+ * 作用：监控store的变化，同时更新subscription
+ */
+export function Provider({ store, context, children }) {
+  const Context = context || ReactReduxContext;
+  const contextValue = useMemo(() => ({ store }), [store]);
+  return <Context.Provider value={contextValue}>{children}</Context.Provider>;
+}
+
+/**
+ * connect 高阶函数
+ * 接收参数：mapStateToProps, mapDispatchToProps
+ * 返回：一个接收组件的高阶函数，此函数返回一个函数式组件
+ * 作用：把state和dispatch绑定到wrapped组件的props上
+ */
+export function connect(mapStateToProps, mapDispatchToProps) {
+  return function (WrappedComponent) {
+    // 组件挂载后，需要监听变化
+    function listen({ store, props, dispatchToUse, setState }) {
+      store.subscribe(() => {
+        const newStateProps = mapStateToProps
+          ? mapStateToProps(store.getState(), props)
+          : {};
+        const newDispatchProps = mapDispatchToProps
+          ? mapDispatchToProps(dispatchToUse, props)
+          : {};
+        const newMergeProps = Object.assign(
+          {},
+          newStateProps,
+          newDispatchProps,
+          props
+        );
+        setState(newMergeProps);
+      });
+    }
+    return function ConnectedComponent(props) {
+      const { store } = React.useContext(ReactReduxContext);
+      const dispatchToUse = (action) => {
+        store.dispatch(action);
+      };
+
+      const initialStateProps = mapStateToProps
+        ? mapStateToProps(store.getState(), props)
+        : {};
+      const initialDispatchProps = mapDispatchToProps
+        ? mapDispatchToProps(dispatchToUse, props)
+        : {};
+      const initialMergeProps = Object.assign(
+        {},
+        initialStateProps,
+        initialDispatchProps,
+        props
+      );
+      const [useProps, setState] = useState(initialMergeProps);
+      useEffect(() => {
+        listen({ store, props, dispatchToUse, setState });
+      });
+      return <WrappedComponent {...useProps} />;
+    };
+  };
+}
